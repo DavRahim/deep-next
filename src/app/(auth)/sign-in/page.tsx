3fb7a3bf7e3d3b7043ae094a0c280152cf2ff2af
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-06-05 22:18:40 +0600

    some bug fix for next

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,7 +3,12 @@ import React from "react";
 type Props = {};
 
 const Page = (props: Props) => {
-  return <div>Page</div>;
+  return ( 
+  <div className="flex justify-center items-center min-h-screen bg-gray-800">
+
+  </div>
+  
+);
 };
 
 export default Page;
