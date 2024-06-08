55b2e4745c32ec9168294ba3130f63e2d6ea9a37
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-06-08 18:20:39 +0600

    some bug fix for new bug

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -5,6 +5,9 @@ type Props = {};
 const Page = (props: Props) => {
   return ( 
   <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        
+      </div>
 
   </div>
   
