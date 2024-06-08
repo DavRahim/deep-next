4ead538694eb4cff5bb561399d526cfe079c1cc3
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-06-08 18:21:30 +0600

    some bug fix for new bug

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,15 +3,19 @@ import React from "react";
 type Props = {};
 
 const Page = (props: Props) => {
-  return ( 
-  <div className="flex justify-center items-center min-h-screen bg-gray-800">
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-        
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join True Feedback
+          </h1>
+          <p className="mb-4">Sign up to start your anonymous adventure</p>
+        </div>
       </div>
+    </div>
 
-  </div>
-  
-);
+  );
 };
 
 export default Page;
